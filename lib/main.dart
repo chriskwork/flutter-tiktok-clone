db3b52cdb55b66de6104ddca1c2dff31218e749b
chris k <yovirinc@gmail.com>
2023-02-27 21:00:39 +0100

    flutter create..

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_clone/screens/sign_up/sign_up_screen.dart';
+
+void main() {
+  runApp(const TikTokApp());
+}
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TikTok Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xFFE9435A),
+        useMaterial3: true,
+      ),
+      home: SignUpScreen(),
+    );
+  }
+}
